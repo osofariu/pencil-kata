3e5d1a1049cd3bc33dda1466e5aff3f862dd2cc7
Ovi Sofariu <ovi@sofariu.com>
2021-12-20 20:18:16 -0500

    test - pencil writes someting, beforeEach in tests

--- src/pencil.test.ts
+++ src/pencil.test.ts
@@ -1,11 +1,23 @@
 import {Pencil} from "./pencil";
 
 describe('Pencil', () => {
+    let pencil: Pencil
+
+    beforeEach(() => {
+        pencil = new Pencil()
+    })
 
     it('writes nothing when no text was given', () => {
         const pencil = new Pencil()
         pencil.write('')
 
         expect(pencil.text).toEqual('')
+    })
+
+    it('writes text when given', () => {
+        pencil.write('Hello')
+
+        expect(pencil.text).toEqual('hello')
+
     })
 })
