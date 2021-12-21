0d7f70ae248792a7d1ec47f6992044a043b6cdf9
Ovi Sofariu <ovi@sofariu.com>
2021-12-20 20:31:28 -0500

    test - pencil degrades; multiple describes

--- src/pencil.test.ts
+++ src/pencil.test.ts
@@ -3,28 +3,43 @@ import {Pencil} from "./pencil";
 describe('Pencil', () => {
     let pencil: Pencil
 
-    beforeEach(() => {
-        pencil = new Pencil()
+    describe('writes text', () => {
+
+        beforeEach(() => {
+            pencil = new Pencil()
+        })
+
+        it('writes nothing when no text was given', () => {
+            const pencil = new Pencil()
+            pencil.write('')
+
+            expect(pencil.text).toEqual('')
+        })
+
+        it('writes text when given', () => {
+            pencil.write('hello')
+
+            expect(pencil.text).toEqual('hello')
+
+        })
+
+        it('writes text multiple times', () => {
+            pencil.write('Hello')
+            pencil.write(' there')
+
+            expect(pencil.text).toEqual('Hello there')
+        })
     })
 
-    it('writes nothing when no text was given', () => {
-        const pencil = new Pencil()
-        pencil.write('')
+    describe('Point degradation', () => {
+        beforeEach(() => {
+            pencil = new Pencil({degrade: 5})
+        })
 
-        expect(pencil.text).toEqual('')
-    })
+        it('degrades by one point for lowercase letters and stops writing', () => {
+            pencil.write('profit')
 
-    it('writes text when given', () => {
-        pencil.write('hello')
-
-        expect(pencil.text).toEqual('hello')
-
-    })
-
-    it('writes text multiple times', () => {
-        pencil.write('Hello')
-        pencil.write(' there')
-
-        expect(pencil.text).toEqual('Hello there')
+            expect(pencil.text).toEqual('profi ')
+        })
     })
 })
